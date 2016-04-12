f92aa6ee4c965eeb799ad1585ce14f07dd88c67a
Bertram Ludaescher <ludaesch@illinois.edu>
2016-04-12 07:26:14 -0500

    dlv2dot now creates out.gv from Saumen's out.txt

--- demo/nwyw/dlv2dot.pl
+++ demo/nwyw/dlv2dot.pl
@@ -13,8 +13,6 @@ go :-
 	run_dlv,
 	read_dlv(Xs),
 	sort(Xs,Ys),
-	% dlv_program(DLV_file),
-	% atom_concat(DLV_file, '.dot', Dot_file),
 	Dot_file = 'out.gv',
 	gv_start(Dot_file),
 	print_dlv(Ys),
@@ -45,9 +43,8 @@ print_dlv(Xs) :-
 	;
 	true.
 
-portray(out(S1,_S2,_S3, T1,_T2,_T3))  :-
-	X=S1, Y=T1,
-	gv_edge(X,2,Y).
+portray(out(S1,S2,S3, T1,T2,T3))  :-
+	gv_edge(S1,S2,S3, T1,T2,T3).
 portray(T) :-
 	T \= out(_,_,_,_,_,_).
 
@@ -64,15 +61,20 @@ gv_start(FileName):-
 	tell(FileName),
 	format('digraph {~n'),
 	format('rankdir=BT~n'),
-	format('ranksep=0.35~n'),
-	format('node [shape=box style="filled,rounded" fixedsize=true color = "#000000" fillcolor="#F8F8F8" width=0.4 height=0.3]~n'),
-	format('edge [fontsize=9]~n').
+%	format('ranksep=0.35~n'),
+	format('node [shape=box style="filled,rounded" color = "#000000" fillcolor="#F8F8F8"]~n'),
+	format('edge [fontsize=9 style=dashed]~n').
 
 % finish graph and close file
 gv_stop:-
 	format('}'),
 	told.
 
-gv_edge(From, _L, To):-
-	format('~w -> ~w [color="#000000" fontcolor="#00AA00"]~n', [From, To]).
+gv_edge(Id1,A1s,V1s, Id2,A2s,V2s):-
+	atom_codes(A1, A1s),
+	atom_codes(V1, V1s),
+	atom_codes(A2, A2s),
+	atom_codes(V2, V2s),
+	format( '"~w:\\n~w=~w" -> "~w:\\n~w=~w" [color="#000000" fontcolor="#00AA00"]~n', 
+	        [Id1,A1,V1, Id2,A2,V2] ).
 
